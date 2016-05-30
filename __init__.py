7448bb21612518094966d0daccd19052c906f893
Ferry Jérémie <ferryjeremie@free.fr>
2016-05-30 23:00:24 +0200

    new version with compatibilty with python 2.7 and python 3.3 and greater

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 """Ace_Editor adapt to a Pelican's plugin."""
 __title__ = 'ace_editor'
-__version__ = '0.2'
+__version__ = '0.2.1'
 __author__ = 'Jérémie Ferry'
 __email__ = "jerem.ferry@gmail.com"
 
